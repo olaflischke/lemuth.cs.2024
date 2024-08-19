7af7954f2e216efa4a3ce1fbc5943f600beda63e
Olaf Lischke <olaf.lischke@lischke-edv.de>
2024-08-19 15:22:18 +0200

    RSS-Reader

--- WPF/Live Coding/RssReader/RssReaderUi/MainWindow.xaml.cs
+++ WPF/Live Coding/RssReader/RssReaderUi/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System.ServiceModel.Syndication;
+﻿using System.Diagnostics;
+using System.ServiceModel.Syndication;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -18,14 +20,42 @@ namespace RssReaderUi
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<SyndicationItem> Items { get; set; }
+        public string TitleText { get; set; }
+        public string Description { get; set; }
+        public string LogoUrl { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
 
+            this.Language = XmlLanguage.GetLanguage(Thread.CurrentThread.CurrentCulture.Name);
+
             XmlReader xmlReader = XmlReader.Create("https://www.spiegel.de/schlagzeilen/tops/index.rss");
+            //XmlReader xmlReader = XmlReader.Create("https://www.insuedthueringen.de/topmeldung.rss2.feed");
             SyndicationFeed feed = SyndicationFeed.Load(xmlReader);
 
-            this.DataContext = feed;
+            this.Items = feed.Items.OrderByDescending(el => el.PublishDate).ToList();
+            this.TitleText = feed.Title.Text;
+            this.Description = feed.Description.Text;
+            this.LogoUrl = feed.ImageUrl.ToString();
+
+            this.DataContext = this;
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            // .NET 4.8 und früher
+            //Process.Start(e.Uri.AbsoluteUri);
+
+            // .NET (Core)
+            ProcessStartInfo startInfo = new ProcessStartInfo()
+            {
+                UseShellExecute = true,
+                FileName = e.Uri.AbsoluteUri
+            };
+
+            Process.Start(startInfo);
         }
     }
 }
